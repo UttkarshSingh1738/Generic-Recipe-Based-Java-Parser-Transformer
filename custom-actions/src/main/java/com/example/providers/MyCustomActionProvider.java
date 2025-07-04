ef16bf62fb81cb44d17c0d1c512af57d77c9d454
Uttkarsh Singh <uttkarsh.singh2004@gmail.com>
2025-07-04 23:29:25 +0530

    refactor

--- custom-actions/src/main/java/com/example/providers/MyCustomActionProvider.java
+++ custom-actions/src/main/java/com/example/providers/MyCustomActionProvider.java
@@ -1,6 +1,8 @@
-package com.example.actions;
+package com.example.providers;
 
 import java.util.Map;
+
+import com.example.actions.MyCustomAction;
 
 import gst.engine.actions.Action;
 import gst.engine.actions.spi.ActionProvider;
